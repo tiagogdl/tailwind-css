dfb18f6bdf076d61b61aa3d3a85a14e8f34802ce
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-05-24 14:39:54 -0300

    Estudos de Tailwind Css

---
+++ nuxt-app/app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="w-full flex gap-3 items-center flex-col tg:flex-row justify-center">
+      <div class="bg-blue-700 w-32 h-32  md:w-64 md:h-64"></div>
+      <div class="bg-blue-700 w-32 h-32  md:w-64 md:h-64"></div>
+      <div class="bg-blue-700 w-32 h-32  md:w-64 md:h-64"></div>
+    </div>
+
+    
+  </div>
+</template>
